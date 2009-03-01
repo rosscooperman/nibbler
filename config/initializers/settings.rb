c90ec912d59b31004c695532fbd51bfb834501e0
Scott Taylor <scott@railsnewbie.com>
2009-03-01 12:17:15 -0500

    Add settings.yml

---
+++ config/initializers/settings.rb
@@ -0,0 +1,24 @@
+unless defined?(SETTINGS)
+  settings_yml = File.join(RAILS_ROOT, 'config', 'settings.yml')
+  unless File.exists?(settings_yml)
+    raise RuntimeError, "Unable to find \"config/settings.yml\" file."
+  end
+
+  SETTINGS = YAML.load_file(settings_yml).recursively_symbolize_keys[RAILS_ENV.to_sym]
+
+  unless SETTINGS.key?(:app_host)
+    raise RuntimeError, "SETTINGS requires :app_host key"
+  end
+
+  ActionMailer::Base.default_url_options[:host] = SETTINGS[:app_host]
+
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  if SETTINGS.key?(:asset_host)
+    ActionController::Base.asset_host = SETTINGS[:asset_host]
+  end
+
+  # Set Image Science INLINEDIR environment variable. Only really necessary for EngineYard.
+  if SETTINGS.key?(:image_science_inlinedir)
+    ENV['INLINEDIR'] = SETTINGS[:image_science_inlinedir]
+  end
+end
