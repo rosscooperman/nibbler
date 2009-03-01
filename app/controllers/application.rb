0dc8ff607a7aaac779ca4e7fd64df769b073951d
Scott Taylor <scott@railsnewbie.com>
2009-03-01 17:43:18 -0500

    Revert "protect from forgery"
    This reverts commit 88112cf3ef994881e6de7184509841829c67c9ad.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + "/../helpers/list_forms"
 
 class ApplicationController < ActionController::Base
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'c9da07f5d587b5917b6d39f307aa962c'
-
   include SharedControllerBehavior
 
   before_filter :login_from_cookie
