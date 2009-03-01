88112cf3ef994881e6de7184509841829c67c9ad
Scott Taylor <scott@railsnewbie.com>
2009-03-01 17:38:03 -0500

    protect from forgery

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'c9da07f5d587b5917b6d39f307aa962c'
+
   include SharedControllerBehavior
 
   before_filter :login_from_cookie
