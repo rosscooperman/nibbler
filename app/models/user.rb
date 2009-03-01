398654cd3cf52df32f016c266c93f994728a9a30
Scott Taylor <scott@railsnewbie.com>
2009-03-01 12:55:18 -0500

    Run migration for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+# Schema version: 20090105200818
+#
+# Table name: users
+#
+#  id                        :integer         not null, primary key
+#  username                  :string(255)
+#  email                     :string(255)
+#  crypted_password          :string(40)
+#  salt                      :string(40)
+#  remember_token            :string(255)
+#  remember_token_expires_at :datetime
+#  created_at                :datetime
+#  updated_at                :datetime
+#  time_zone                 :string(255)
+#
+
 class User < ActiveRecord::Base
   include Authenticated
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090105200818) do
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "time_zone"
+  end
 
 end
