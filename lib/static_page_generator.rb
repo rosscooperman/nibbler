9e4c3d29fbe5bac13dcca3c2a4abed3eba9ca6ac
Matt Pelletier <matt@eastmedia.com>
2009-04-06 18:13:40 -0400

    Add ability to use http auth for static template generation

--- lib/static_page_generator.rb
+++ lib/static_page_generator.rb
@@ -5,7 +5,12 @@ class StaticPageGenerator
 
   def generate
     templates.each do |template_name|
-      sh "curl http://#{SETTINGS[:app_host]}/static/#{template_name} > #{RAILS_ROOT}/public/#{template_name}.html"
+      auth = if SETTINGS.key?(:http_basic_auth) && SETTINGS[:http_basic_auth].key?(:user) && SETTINGS[:http_basic_auth].key?(:password)
+        " -u #{SETTINGS[:http_basic_auth][:user]}:#{SETTINGS[:http_basic_auth][:password]} "
+      else
+        ""
+      end
+      sh "curl #{auth} http://#{SETTINGS[:app_host]}/static/#{template_name} > #{RAILS_ROOT}/public/#{template_name}.html"
     end
   end
 
