90b63e27b00386ed17152edb111c5928e1fc06e7
Mike Bueti <mike@eastmedia.com>
2009-04-07 11:09:45 -0400

    Braid: Updated mirror 'vendor/plugins/oink' to '83bc8bf'

--- vendor/plugins/oink/lib/oink/rails/instance_type_counter.rb
+++ vendor/plugins/oink/lib/oink/rails/instance_type_counter.rb
@@ -1,4 +1,13 @@
 module Oink
+  
+  def self.extended_active_record?
+    @oink_extended_active_record
+  end
+
+  def self.extended_active_record!
+    @oink_extended_active_record = true
+  end
+  
   module InstanceTypeCounter
     def self.included(klass)
       ActiveRecord::Base.send(:include, OinkInstanceTypeCounterInstanceMethods)
@@ -45,18 +54,31 @@ module Oink
         def self.total_objects_instantiated
           @@total ||= @@instantiated.inject(0) { |i, j| i + j.last }
         end
+
+        unless Oink.extended_active_record?
+          if instance_methods.include?("after_initialize")
+            def after_initialize_with_oink
+              after_initialize_without_oink
+              increment_ar_count
+            end
+          
+            alias_method_chain :after_initialize, :oink
+          else
+            def after_initialize
+              increment_ar_count
+            end
+          end
+          
+          Oink.extended_active_record!
+        end
       
       end
     end
   
-    def after_initialize
+    def increment_ar_count
       @@instantiated[self.class.base_class.name] ||= 0
       @@instantiated[self.class.base_class.name] = @@instantiated[self.class.base_class.name] + 1  
     end
-  
-    def after_initialize_with_instance_type_count
-      after_initialize_without_instance_type_count
-      _instance_counter_after_initialize
-    end
+
   end
 end
