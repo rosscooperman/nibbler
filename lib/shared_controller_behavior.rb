c2d62c75c4ca1469d7dd23619add8f9ba042ed70
Scott Taylor <scott@railsnewbie.com>
2009-04-11 15:49:03 -0400

    Fix indentation

--- lib/shared_controller_behavior.rb
+++ lib/shared_controller_behavior.rb
@@ -1,32 +1,31 @@
- module SharedControllerBehavior
-   def self.included(controller)
-     controller.class_eval do
-       include Oink::MemoryUsageLogger
-       include Oink::InstanceTypeCounter
+module SharedControllerBehavior
+  def self.included(controller)
+    controller.class_eval do
+      include Oink::MemoryUsageLogger
+      include Oink::InstanceTypeCounter
 
-       include ExceptionLoggable
-       include AuthenticatedSystem
-       include Authorization
-       include FlashMessages
+      include ExceptionLoggable
+      include AuthenticatedSystem
+      include Authorization
+      include FlashMessages
 
-       helper_method :app_host
-       helper :shared
-       helper :images
+      helper_method :app_host
+      helper :shared
+      helper :images
 
-       attr_accessor :current_tab
+      attr_accessor :current_tab
 
-       filter_parameter_logging :password
+      filter_parameter_logging :password
 
-       # See ActionController::RequestForgeryProtection for details
-       # Uncomment the :secret if you're not using the cookie session store
-       protect_from_forgery # :secret => 'c9da07f5d587b5917b6d39f307aa962c'
-     end
-   end
+      # See ActionController::RequestForgeryProtection for details
+      # Uncomment the :secret if you're not using the cookie session store
+      protect_from_forgery # :secret => 'c9da07f5d587b5917b6d39f307aa962c'
+    end
+  end
 
- protected
+protected
 
-   def app_host
-     SETTINGS[:app_host]
-   end
-
- end
+  def app_host
+    SETTINGS[:app_host]
+  end
+end
