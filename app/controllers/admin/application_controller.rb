782fe8bd2220e079e0c47a60ea6fee97ec60305e
Scott Taylor <scott@railsnewbie.com>
2009-05-07 11:29:05 -0400

    Remove reference to breadcrumb helper (which doesn't exist)

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,12 +4,8 @@ class Admin::ApplicationController < ActionController::Base
   before_filter :login_required unless RAILS_ENV == 'test'
   before_filter :login_from_cookie
 
-  # before_filter :ensure_is_admin
-
   filter_parameter_logging :password
 
-  helper :breadcrumbs
-  
   attr_accessor :current_tab
   
   layout "application"
