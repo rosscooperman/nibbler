378e9f6ef56927c0da0f58be665614970bb63db8
jacqui <jacqui@numbersix.local>
2009-07-02 11:15:34 -0400

    Remove Grit gem dependency and associated helper methods

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -3,9 +3,9 @@ module SharedHelper
     output = ""
     names.each do |name|
       if content = flash[name]
-        output << content_tag(:p, content, :class => "flash #{name}", :id => "flash_#{name}")
-        # output << javascript_tag(visual_effect(:fade, "flash_#{name}", :duration => 30))
-        flash.discard(name)
+  output << content_tag(:p, content, :class => "flash #{name}", :id => "flash_#{name}")
+  # output << javascript_tag(visual_effect(:fade, "flash_#{name}", :duration => 30))
+  flash.discard(name)
       end
     end
     output
@@ -14,7 +14,7 @@ module SharedHelper
   def strip_ampersands(string)
     (string || "").gsub(/&amp;/xim, '&')
   end
-  
+
   def body_tag
     body = "<body"
     body << " id=\"#{@body_id || controller.controller_name}\""
@@ -30,22 +30,14 @@ module SharedHelper
     SETTINGS[:app_host]
   end
 
-  def git_version_html
-    @git_version_html ||= "<!-- git master:#{git_version} -->"
-  end
-
-  def git_version
-    Grit::Repo.new(RAILS_ROOT).commits.first.id
-  end
-
   def staging?
     SETTINGS[:staging]
   end
-  
+
   def admin?
     controller.controller_path.include? "admin/"
   end
-  
+
   def public?
     !admin?
   end
