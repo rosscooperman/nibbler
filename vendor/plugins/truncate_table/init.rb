68ea6aab2bbf45c67cb71ec0c8e68ef15e94b233
Scott Taylor <scott@railsnewbie.com>
2009-11-06 16:02:35 -0500

    Braid: Removed mirror 'vendor/plugins/truncate_table'

--- vendor/plugins/truncate_table/init.rb
+++
@@ -1,4 +0,0 @@
-
-require File.dirname(__FILE__) + "/lib/truncate_table"
-
-ActiveRecord::Base.extend TruncateTable
--- vendor/plugins/truncate_table/spec/version_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + "/spec_helper"
-
-describe TruncateTable, "'s version" do
-  it "should be at 1.0.0" do
-    TruncateTable::Version::VERSION.should == "1.0.0"
-  end
-end
