dd57405c1864b381f8ab344230a62661eb84ba5f
Scott Taylor <scott@railsnewbie.com>
2010-01-05 15:28:53 -0500

    Upgrade rspec + rspec-rails to latest with script/generate rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
 
@@ -12,14 +12,5 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helpers")
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-
-  config.include FixtureReplacement
-  config.include SpecHelpers
-  config.include SpecHelpers::ControllerHelpers, :type => :controller
-
-  Dir.glob(File.join(File.dirname(__FILE__), "matchers", "*_matchers.rb")).each do |matcher|
-    require matcher
-    config.include File.basename(matcher, ".rb").camelize.constantize
-  end
+  config.fixture_path               = nil # fixtures are evil
 end
