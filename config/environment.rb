bf1013354c61f55749307d85d26744d9fd5d084c
Scott Taylor <scott@railsnewbie.com>
2010-01-05 15:31:20 -0500

    Convert tabs to spaces

--- config/environment.rb
+++ config/environment.rb
@@ -11,60 +11,60 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-	# Settings in config/environments/* take precedence over those specified here.
-	# Application configuration should go into files in config/initializers
-	# -- all .rb files in that directory are automatically loaded.
-	# See Rails::Configuration for more options.
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  # See Rails::Configuration for more options.
 
-	# Skip frameworks you're not going to use. To use Rails without a database
-	# you must remove the Active Record framework.
-	# config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  # Skip frameworks you're not going to use. To use Rails without a database
+  # you must remove the Active Record framework.
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-	# Specify gems that this application depends on.
-	# They can then be installed with "rake gems:install" on new installations.
-	# config.gem "bj"
-	# config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-	# config.gem "aws-s3", :lib => "aws/s3"
+  # Specify gems that this application depends on.
+  # They can then be installed with "rake gems:install" on new installations.
+  # config.gem "bj"
+  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+  # config.gem "aws-s3", :lib => "aws/s3"
 
-	# Only load the plugins named here, in the order given. By default, all plugins
-	# in vendor/plugins are loaded in alphabetical order.
-	# :all can be used as a placeholder for all plugins not explicitly named
-	# config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  # Only load the plugins named here, in the order given. By default, all plugins
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-	# Add additional load paths for your own custom dirs
-	# config.load_paths += %W( #{RAILS_ROOT}/extras )
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-	# Force all environments to use the same logger level
-	# (by default production uses :info, the others :debug)
-	# config.log_level = :debug
+  # Force all environments to use the same logger level
+  # (by default production uses :info, the others :debug)
+  # config.log_level = :debug
 
-	# Make Time.zone default to the specified zone, and make Active Record store time values
-	# in the database in UTC, and return them converted to the specified local zone.
-	# Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
-	config.time_zone = 'UTC'
+  # Make Time.zone default to the specified zone, and make Active Record store time values
+  # in the database in UTC, and return them converted to the specified local zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
+  config.time_zone = 'UTC'
 
-	# Your secret key for verifying cookie session data integrity.
-	# If you change this key, all old sessions will become invalid!
-	# Make sure the secret is at least 30 characters and all random,
-	# no regular words or you'll be exposed to dictionary attacks.
-	config.action_controller.session = {
-		:session_key => '_em_session',
-		:secret			 => 'd4d1ad287d1b38481a5c1feb7ef95bcd6fb25cd71af22b96e5ae6c06c0a8817f1a81a98cfe0a7915774764539dbdad2fbb9993fff21acd2458c6af8920147891'
-	}
+  # Your secret key for verifying cookie session data integrity.
+  # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random,
+  # no regular words or you'll be exposed to dictionary attacks.
+  config.action_controller.session = {
+    :session_key => '_em_session',
+    :secret       => 'd4d1ad287d1b38481a5c1feb7ef95bcd6fb25cd71af22b96e5ae6c06c0a8817f1a81a98cfe0a7915774764539dbdad2fbb9993fff21acd2458c6af8920147891'
+  }
 
-	# Use the database for sessions instead of the cookie-based default,
-	# which shouldn't be used to store highly confidential information
-	# (create the session table with "rake db:sessions:create")
-	# config.action_controller.session_store = :active_record_store
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
+  # (create the session table with "rake db:sessions:create")
+  # config.action_controller.session_store = :active_record_store
 
-	# Use SQL instead of Active Record's schema dumper when creating the test database.
-	# This is necessary if your schema can't be completely dumped by the schema dumper,
-	# like if you have constraints or database-specific column types
-	# config.active_record.schema_format = :sql
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper,
+  # like if you have constraints or database-specific column types
+  # config.active_record.schema_format = :sql
 
-	# Activate observers that should always be running
-	# config.active_record.observers = :cacher, :garbage_collector
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector
 
-	require RAILS_ROOT + "/vendor/plugins/hodel_3000_compliant_logger/lib/hodel_3000_compliant_logger"
-	config.logger = Hodel3000CompliantLogger.new(config.log_path)
+  require RAILS_ROOT + "/vendor/plugins/hodel_3000_compliant_logger/lib/hodel_3000_compliant_logger"
+  config.logger = Hodel3000CompliantLogger.new(config.log_path)
 end
