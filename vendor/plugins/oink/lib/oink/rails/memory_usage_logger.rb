60bba696211a6982654593e28dcac9abc35b6542
Scott Taylor <scott@railsnewbie.com>
2010-01-05 15:44:31 -0500

    Braid: Updated mirror 'vendor/plugins/oink' to 'cbb4562'

--- vendor/plugins/oink/lib/oink/rails/memory_usage_logger.rb
+++ vendor/plugins/oink/lib/oink/rails/memory_usage_logger.rb
@@ -1,3 +1,8 @@
+begin
+  require 'win32ole'
+rescue LoadError
+end
+
 module Oink
   module MemoryUsageLogger
     def self.included(klass)
@@ -7,9 +12,23 @@ module Oink
     end
   
     private
+      def get_memory_usage
+        if defined? WIN32OLE
+          wmi = WIN32OLE.connect("winmgmts:root/cimv2")
+          mem = 0
+          query = "select * from Win32_Process where ProcessID = #{$$}"
+          wmi.ExecQuery(query).each do |wproc|
+            mem = wproc.WorkingSetSize
+          end
+          mem.to_i / 1000
+        else
+          `ps -o rss= -p #{$$}`.to_i
+        end
+      end
+
       def log_memory_usage
         if logger
-          memory_usage = `ps -o rss= -p #{$$}`.to_i
+          memory_usage = get_memory_usage
           logger.info("Memory usage: #{memory_usage} | PID: #{$$}")
         end
       end
