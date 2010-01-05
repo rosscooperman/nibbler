b17eec200de5e31070f0998b08350044eb1524f2
Scott Taylor <scott@railsnewbie.com>
2010-01-05 15:25:16 -0500

    Update rails to 2.3.5

--- vendor/rails/actionpack/lib/action_controller.rb
+++ vendor/rails/actionpack/lib/action_controller.rb
@@ -31,12 +31,9 @@ rescue LoadError
   end
 end
 
-begin
-  gem 'rack', '~> 1.0.0'
-  require 'rack'
-rescue Gem::LoadError
-  require 'action_controller/vendor/rack-1.0.x/rack'
-end
+gem 'rack', '~> 1.0.1'
+require 'rack'
+require 'action_controller/cgi_ext'
 
 module ActionController
   # TODO: Review explicit to see if they will automatically be handled by
@@ -74,6 +71,7 @@ module ActionController
   autoload :SessionManagement, 'action_controller/session_management'
   autoload :StatusCodes, 'action_controller/status_codes'
   autoload :Streaming, 'action_controller/streaming'
+  autoload :StringCoercion, 'action_controller/string_coercion'
   autoload :TestCase, 'action_controller/test_case'
   autoload :TestProcess, 'action_controller/test_process'
   autoload :Translation, 'action_controller/translation'
