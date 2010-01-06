24e1b01084768dd01a4c63cafde2e4cfaecdf5e6
Scott Taylor <scott@railsnewbie.com>
2010-01-06 16:51:06 -0500

    Whitespace (rebase me)

--- app/mailers/contact_submission_mailer.rb
+++ app/mailers/contact_submission_mailer.rb
@@ -1,7 +1,7 @@
 class ContactSubmissionMailer < ActionMailer::Base
   def submission(submission, sent_on = Time.now.utc)
-    from "#{submission.name} <#{SETTINGS[:email][:contact_submission_recipient]}>"
-    sent_on sent_on
+    from        "#{submission.name} <#{SETTINGS[:email][:contact_submission_recipient]}>"
+    sent_on     sent_on
     reply_to    "#{submission.email}"
     recipients  SETTINGS[:email][:contact_submission_recipient]
     subject     submission.subject
