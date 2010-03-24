b73a2c5cedb9f7a0cf9b1912173d095c2d66b71c
Scott Taylor <scott@railsnewbie.com>
2010-03-24 14:51:39 -0400

    Update oink to latest version (4e375b02dd1a7c0298924417b81fbf119a48f4f5)

--- vendor/plugins/oink/lib/oink/memory_usage_reporter.rb
+++ vendor/plugins/oink/lib/oink/memory_usage_reporter.rb
@@ -6,7 +6,6 @@ require "oink/priority_queue"
 module Oink
 
   class MemoryUsageReporter < Base
-
     def print(output)
       output.puts "---- MEMORY THRESHOLD ----"
       output.puts "THRESHOLD: #{@threshold/1024} MB\n"
@@ -16,6 +15,9 @@ module Oink
       @inputs.each do |input|
         input.each_line do |line|
           line = line.strip
+          
+           # Skip this line since we're only interested in the Hodel 3000 compliant lines
+          next unless line =~ HODEL_LOG_FORMAT_REGEX
 
           if line =~ /rails\[(\d+)\]/
             pid = $1
@@ -44,7 +46,7 @@ module Oink
               if memory_diff > @threshold
                 @bad_actions[@pids[pid][:action]] ||= 0
                 @bad_actions[@pids[pid][:action]] = @bad_actions[@pids[pid][:action]] + 1
-                date = /^(\w+ \d{2} \d{2}:\d{2}:\d{2})/.match(line).captures[0]
+                date = HODEL_LOG_FORMAT_REGEX.match(line).captures[0]
                 @bad_requests.push(OinkedMemoryRequest.new(@pids[pid][:action], date, @pids[pid][:buffer], memory_diff))
                 if @format == :verbose
                   @pids[pid][:buffer].each { |b| output.puts b } 
