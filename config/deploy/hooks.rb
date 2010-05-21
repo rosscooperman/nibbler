82aea2722bfbadf3d0f608e89aeeb538367bef1e
Scott Taylor <scott@railsnewbie.com>
2010-05-21 16:19:47 -0400

    remove task which doesn't exist: symlink_configs

--- config/deploy/hooks.rb
+++ config/deploy/hooks.rb
@@ -1,4 +1,4 @@
-after "deploy:symlink_configs", "deploy:symlink_app_configs"
+after "deploy:symlink", "deploy:symlink_app_configs"
 
 common_after_deploy_tasks = [
   "deploy:tag",
@@ -9,5 +9,4 @@ common_after_deploy_tasks = [
 after   "deploy",                 *common_after_deploy_tasks
 after   "deploy:migrations",      *common_after_deploy_tasks
 after   "deploy:long",            *common_after_deploy_tasks
-after   "deploy:update_code",     "deploy:symlink_configs"
 after   "deploy:setup",           "deploy:setup_directory_permissions", "deploy:setup_shared_directories"
