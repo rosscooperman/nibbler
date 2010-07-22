c2375432a9fc65b69ab158b47999134df16d5835
jeffboulet <jeff.boulet@gmail.com>
2010-07-22 17:43:59 -0400

    Align users controller with pages controller for indexes - comment out code

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,10 @@ class Admin::UsersController < Admin::ApplicationController
   make_resourceful do
     actions :all
   end
+  
+  def index
+    # @index = UsersIndex.new(params)
+    # @users = @index.paginate
+  end
+  
 end
