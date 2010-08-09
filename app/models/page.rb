43c8beeef5fdc45313d4373ba315b5a307654eb2
Scott Taylor <scott@railsnewbie.com>
2010-08-09 14:17:33 -0400

    annotations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,30 +1,19 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id         :integer(4)      not null, primary key
+#  title      :string(255)
+#  slug       :string(255)
+#  body       :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Page < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :slug
   validates_presence_of :body
   validates_uniqueness_of :slug
 end
-
-# == Schema Information
-#
-# Table name: pages
-#
-#  id         :integer(4)      not null, primary key
-#  title      :string(255)
-#  slug       :string(255)
-#  body       :text
-#  created_at :datetime
-#  updated_at :datetime
-#
-# == Schema Information
-#
-# Table name: pages
-#
-#  id         :integer(4)      not null, primary key
-#  title      :string(255)
-#  slug       :string(255)
-#  body       :text
-#  created_at :datetime
-#  updated_at :datetime
-#
 
