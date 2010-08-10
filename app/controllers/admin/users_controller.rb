6ec85c738568df9fa03c4b5a12848bee3566a4df
Scott Taylor <scott@railsnewbie.com>
2010-08-10 14:52:11 -0400

    fix users index in admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,16 @@
 class Admin::UsersController < Admin::ApplicationController
   before_filter { |c| c.current_tab = :users }
-  
+
   make_resourceful do
-    actions :all
+    actions :new, :create, :edit, :update, :destroy
+
+    response_for :create, :update, :destroy do
+      redirect_to admin_users_path
+    end
   end
-  
+
   def index
-    # @index = UsersIndex.new(params)
-    # @users = @index.paginate
+    @index = UsersIndex.new(params)
+    @users = @index.paginate
   end
-  
 end
---
+++ app/indexes/users_index.rb
@@ -0,0 +1,21 @@
+class UsersIndex < IndexView::Base
+  column :email
+  column :edit, :link => lambda { |admin|
+    returning String.new do |text|
+      text << link_to("Edit", edit_admin_user_path(admin))
+      text << "&nbsp;"
+
+      unless current_user == admin
+        text << link_to("Remove", admin_user_path(admin), :method => :delete)
+      end
+    end
+  }
+
+  def target_class
+    User
+  end
+
+  def default_sort_term
+    :email
+  end
+end
