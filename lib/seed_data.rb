392af8206ab1c1ca9436163f2f2164c469981905
Scott Taylor <scott@railsnewbie.com>
2010-08-10 13:09:45 -0400

    create example admin in seeds

---
+++ lib/seed_data.rb
@@ -0,0 +1,19 @@
+module SeedData
+  extend FixtureReplacement
+  extend self
+
+  def seed
+    create_admins
+  end
+
+  def create_admins
+    if Rails.env.staging? || Rails.env.development?
+      begin
+        puts "* Creating example admin"
+        create_example_admin
+      rescue ActiveRecord::RecordInvalid
+        puts "* Skipping creation of example admin"
+      end
+    end
+  end
+end
