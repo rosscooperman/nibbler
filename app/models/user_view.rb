62a0f6b0009022558c88baa439acbedc4a746ad8
nodanaonlyzuul <stephen@eastmedia.com>
2010-09-29 16:58:37 -0400

    re-adding annotate models plugin

--- app/models/user_view.rb
+++ app/models/user_view.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: user_view
+#
+#  id                        :integer(4)      default(0), not null, primary key
+#  email                     :string(255)
+#  crypted_password          :string(40)
+#  salt                      :string(40)
+#  remember_token            :string(255)
+#  remember_token_expires_at :datetime
+#  time_zone                 :string(255)
+#  type                      :string(255)
+#  created_at                :datetime
+#  updated_at                :datetime
+#
+
 class UserView < ActiveRecord::Base
   set_table_name "user_view"
 end
