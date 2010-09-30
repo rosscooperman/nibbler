578e4766b455884c37cf822cbea414a42f154163
nodanaonlyzuul <stephen@eastmedia.com>
2010-09-30 02:42:14 -0400

    changing RAILS_ROOT to Rails.root

--- lib/flash_messages.rb
+++ lib/flash_messages.rb
@@ -5,7 +5,7 @@ protected
     flash_type, message = extract_flash_message(page, message, *values)
     flash[flash_type] = message
   end
-  
+
   def flash_message_now(page, message, *values)
     flash_type, message = extract_flash_message(page, message, *values)
     flash.now[flash_type] = message
@@ -15,16 +15,16 @@ protected
     message = flash_messages_from_yaml[page] && flash_messages_from_yaml[page][message_type]
     raise "Unknown flash message specified: #{page}/#{message_type}" if message.nil?
     flash_type = :notice
-  
+
     if message.is_a?(Hash)
       flash_type = message.keys.first
       message = message.values.first
     end
-    
+
     [flash_type, sprintf(message, *values)]
   end
-  
+
   def flash_messages_from_yaml
-    @flash_messages_from_yaml ||= YAML.load_file(File.join(RAILS_ROOT, "lib", "flash_messages.yml")).recursively_symbolize_keys
+    @flash_messages_from_yaml ||= YAML.load_file(File.join(RAILS.root, "lib", "flash_messages.yml")).recursively_symbolize_keys
   end
 end
