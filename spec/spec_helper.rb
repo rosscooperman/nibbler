3590b80dfda45c025a3b851715b02f8503d5458f
nodanaonlyzuul <stephen@eastmedia.com>
2010-10-04 22:34:32 -0400

    config.include ExampleHelpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
   config.include FixtureReplacement
+  config.include ExampleHelpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
