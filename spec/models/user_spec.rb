6d5b7fe42452e3e50285c1f7d38f4812485c8fe1
nodanaonlyzuul <stephen@eastmedia.com>
2010-10-04 22:37:54 -0400

    removing annotation from spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,22 +42,3 @@ describe User do
     end
   end
 end
-
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id                        :integer(4)      not null, primary key
-#  username                  :string(255)
-#  email                     :string(255)
-#  crypted_password          :string(40)
-#  salt                      :string(40)
-#  remember_token            :string(255)
-#  remember_token_expires_at :datetime
-#  time_zone                 :string(255)
-#  type                      :string(255)
-#  created_at                :datetime
-#  updated_at                :datetime
-#
-
