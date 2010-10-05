921cee3386a147b0fd537a7fe9d53ec6dc78b299
nodanaonlyzuul <stephen@eastmedia.com>
2010-10-05 09:31:31 -0400

    Adding from address used in test emails

--- spec/fixtures/settings.rb
+++ spec/fixtures/settings.rb
@@ -1,7 +1,7 @@
 SETTINGS = {
   :app_host   => "localhost:3000",
   :email      => {
-    :from => "",
+    :from => "fromaddress@example.com",
     :contact_submission_recipient => "test@example.com"
   }
 } unless defined?(SETTINGS)
