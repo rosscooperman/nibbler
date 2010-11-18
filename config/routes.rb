731a43e1b735f0a7ce1aded5125c6dd11925d8a2
Matt Pelletier <matt@eastmedia.com>
2010-11-18 16:37:51 -0500

    Use consistent quoting for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # For more Rails 3 Routing info: http://www.engineyard.com/blog/2010/the-lowdown-on-routes-in-rails-3/
 # or generate see auto-generated routes.rb file in a test application
 FreshRailsApp::Application.routes.draw do
-
   # Admin Section
   namespace :admin do
     resources :users
@@ -19,11 +18,11 @@ FreshRailsApp::Application.routes.draw do
   root :to => "home#index"
 
   # short-hand for: match  'sign_out', :to => "session#destroy"
-  match  'sign_out'                 => "session#destroy"
+  match  "sign_out"                 => "session#destroy"
   match  "signed_out"               => "session#signed_out"
-  match  "reset_password/:id/:hash" => 'session#reset_password', :as => "reset_password"
-  match  'forgot_password'          => 'session#forgot_password'
-  match  'sign_in'                  => "session#new"
+  match  "reset_password/:id/:hash" => "session#reset_password", :as => "reset_password"
+  match  "forgot_password"          => "session#forgot_password"
+  match  "sign_in"                  => "session#new"
 
   # Health Check
   match "health_check/check" => "health_check#health_check"
