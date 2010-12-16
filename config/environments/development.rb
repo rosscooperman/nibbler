a46141429b013340a579514f774e8c10ca6ad32f
nodanaonlyzuul <stephen@eastmedia.com>
2010-12-16 10:22:28 -0500

    Development environment will not send email and will not raise delivery errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,7 @@ FreshRailsApp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method       = :sendmail
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
