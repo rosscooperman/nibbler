c7be61f47f46c9b4a67cedd0e9a54fc6c7bed2c3
Scott Taylor <scott@railsnewbie.com>
2011-05-16 16:53:48 -0400

    make sure development uses delivery method = :test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ FreshRailsApp::Application.configure do
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :test
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
