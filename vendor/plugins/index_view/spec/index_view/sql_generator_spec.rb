508618bb2f78bfde00502a02f96564b8ed162338
Ross Cooperman <cooperman@gmail.com>
2011-11-01 11:20:19 -0400

    Removed the index_view plugin -- no longer needed since we switched to activeadmin.

--- vendor/plugins/index_view/spec/index_view/sql_generator_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
-
-describe IndexView::SQLGenerator do
-  it "should be a module" do
-    IndexView::SQLGenerator.should be_a_kind_of(Module)
-  end
-end
--- vendor/plugins/index_view/spec/index_view/version_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe IndexView::Version do
-  it "should be at 0.1.0" do
-    IndexView::Version::STRING.should == "0.1.0"
-  end
-end
