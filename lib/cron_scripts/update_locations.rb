0e2c1572b151d2f83a7d8ecc8ce1fc0396707dbf
Ross Cooperman <cooperman@gmail.com>
2011-12-22 22:30:15 -0500

    Fix (hopefully) for rate limiting issues with Twitter.

--- lib/cron_scripts/update_locations.rb
+++ lib/cron_scripts/update_locations.rb
@@ -5,9 +5,10 @@ module CronScripts
     end
 
     def do_action
-      Truck.all.each do |truck|
+      Truck.order('updated_at ASC').limit(4).each do |truck|
         log "Finding new location(s) for '#{truck.name}'"
         truck.update_data_points()
+        truck.touch
       end
     end
 
