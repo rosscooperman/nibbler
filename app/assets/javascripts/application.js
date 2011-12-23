2e9414ef84f2e67c88f24f068676917e34100bc0
Ross Cooperman <cooperman@gmail.com>
2011-12-23 00:48:29 -0500

    Working results list population and show/hide toggle.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,33 @@
 //= require jquery.colorbox-min
 //= require foundation
 //= require app
+//= require handlebars
 //= require map
 //= require mapping
 
-$(document).ready(function(){
+$(function() {
+
+  function toggleResults() {
+    var righty = $(this).next();
+		righty.animate({
+		  right: (parseInt(righty.css('right'), 10) == 0) ? -righty.outerWidth() : 0
+		});
+    $('button.showResults').fadeIn();
+  }
+
+
+  function renderResults(data) {
+    var source   = $('#result-template').html();
+    var template = Handlebars.compile(source);
+    var target   = $('ul.results');
+    var mapIndex = 'A'.charCodeAt(0);
+
+    $.each(data, function() {
+      var html = template({ truck: this, mapIndex: String.fromCharCode(mapIndex++) });
+      target.append(html);
+    });
+  }
+
 
   function handleSearch(event) {
 	  event.preventDefault();
@@ -26,10 +49,12 @@ $(document).ready(function(){
   	      window.theMap.addMarker(this.location.lat, this.location.lng);
   	      window.theMap.zoomToPoint(this.location.lat, this.location.lng);
 	      });
+	      renderResults(data);
 	      $('input[name=q]').val(query);
         $('.noobSearch').fadeOut(300);
         $('.logoMarker').fadeOut(900);
         $('.topWrapper').slideDown('slow');
+        $('button.showResults').click();
 	    },
 	    error:    function(xhr, status, error) {
 	      console.log(error);
@@ -40,19 +65,18 @@ $(document).ready(function(){
 	  });
   }
 
+
   function handleClick(event) {
     event.preventDefault();
     $(this).closest('form').submit();
   }
 
+
+  // handle clicks on the search button(s) and submission of the search form(s)
 	$('.searchButton').click(handleClick);
 	$('.noobSearch form, .topContainer form').submit(handleSearch);
 
+
 	// slide the results in from the right
-	$('button.showResults').click(function() {
-	    var $righty = $(this).next();
-		    $righty.animate({
-		      right: parseInt($righty.css('right'),10) == 0 ? -$righty.outerWidth() : 0 });
-	  });
-	
+	$('button.showResults').click(toggleResults);
 });
--- app/views/trucks/index.json.jbuilder
+++ app/views/trucks/index.json.jbuilder
@@ -1,5 +1,6 @@
 json.array!(@trucks) do |json, truck|
   json.name truck.name
+  json.description truck.description
 
   location = truck.locations.order('id DESC').first
   json.location do |json|
